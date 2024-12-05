0a357f039332b4ae65bb5b672301e9c6851b9a05
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-12-05 15:25:14 -0300

    2024 (pt.2) Day 5: Print Queue

--- js/2024/day-05/solve.js
+++ js/2024/day-05/solve.js
@@ -64,7 +64,54 @@ function getInvalidUpdatePages(reference, update) {
 }
 
 /**
- * Filter for the valid ones, map to get the middle from each them sum it all.
+ * Change the list to swap two indexes from a list and return the updated value.
+ * @param {Array<any>} arr 
+ * @param {number} idx1
+ * @param {number} idx2 
+ */
+function swap(arr, idx1, idx2) {
+  const temp = arr[idx1]
+
+  arr[idx1] = arr[idx2];
+  arr[idx2] = temp;
+
+  return [...arr];
+}
+
+/**
+ * Moves the invalid numbers one step behind until it's all following the rules.
+ * @param {{[id: string]: number[]}} reference
+ * @param {number[]} update 
+ * @returns {number[]}
+ */
+function fixInvalidUpdatePages(reference, update) {
+  let corrected = [...update];
+  let invalids = getInvalidUpdatePages(reference, corrected);
+
+  if (invalids.length === 0)
+    return update;
+
+  while (invalids.length > 0) {
+    // Move every invalid page one position behind.
+    for (const invalid of invalids) {
+      const pos = corrected.indexOf(invalid);
+      const next = pos - 1;
+
+      // Pass to the next invalid page if the next position is out of bounds.
+      if (next < 0)
+        continue;
+
+      swap(corrected, pos, next)
+    }
+
+    invalids = getInvalidUpdatePages(reference, corrected);
+  }
+
+  return corrected;
+}
+
+/**
+ * Filter for the invalid ones, fix them, map to get the middle then sum it all.
  * @param {string} input 
  * @returns {number}
  */
@@ -74,7 +121,9 @@ function solve(input) {
 
   return updates
     .filter((update) =>
-      getInvalidUpdatePages(reference, update).length == 0) // No invalid
+      getInvalidUpdatePages(reference, update).length > 0) // Invalids only
+    .map((update) =>
+      fixInvalidUpdatePages(reference, update))  // Fix the invalids
     .map((update) =>
       update[Math.floor(update.length / 2)]) // Get middle page number
     .reduce((acc, page) => acc + page); // Sum everything
