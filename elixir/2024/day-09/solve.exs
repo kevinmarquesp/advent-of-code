e324e428324e8172297f505cdbcb639ba1ac87c6
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-12-09 15:30:33 -0300

    2024 (pt.2) Day 9: Disk Fragmenter

--- elixir/2024/day-09/solve.exs
+++ elixir/2024/day-09/solve.exs
@@ -2,7 +2,7 @@
 
 defmodule Solve do
   @doc """
-  Generates the alternating lists then transform it to the correct one to get the checksum.
+  Convert to sequency frequency then parse it all to calculate the result.
   """
   def solve(input) when is_bitstring(input) do
     String.split(input, "")
@@ -12,42 +12,74 @@ defmodule Solve do
   end
 
   def solve(input) when is_list(input) do
-    # Alternates between empty block and filled block while tracking the id.
     {_, _, blocks} =
       Enum.reduce(input, {false, 0, []}, fn
         size, {true, id, acc} ->
-          {false, id, acc ++ List.duplicate(nil, size)}
+          {false, id, acc ++ [{:empty, nil, size}]}
 
         size, {false, id, acc} ->
-          {true, id + 1, acc ++ List.duplicate(id, size)}
+          {true, id + 1, acc ++ [{:movable, id, size}]}
       end)
 
-    compressed_size = length(blocks) - Enum.frequencies(blocks)[nil] - 1
+    compress_blocks(blocks)
+    |> Enum.flat_map(fn {_, data, size} -> List.duplicate(data, size) end)
+    |> Enum.reduce({0, 0}, fn
+      nil, {idx, res} -> {idx + 1, res}
+      num, {idx, res} -> {idx + 1, res + idx * num}
+    end)
+    |> elem(1)
+  end
 
-    # Generate the compressed list with the expected size.
-    compressed =
-      0..compressed_size
-      |> Enum.reduce(blocks, fn idx, acc ->
-        if Enum.at(blocks, idx) === nil do
-          # Get the last non nil index.
-          last_idx =
-            (Enum.reverse(acc)
-             |> Enum.find_index(&(&1 !== nil))) * -1 - 1
+  @doc """
+  Uses recursion to move the end files to the free space blocks.
+  """
+  def compress_blocks(blocks) do
+    {files, spaces} =
+      Enum.zip(0..(length(blocks) - 1), blocks)
+      |> Enum.reduce({[], []}, fn
+        {idx, {:empty, nil, space}}, {files, spaces} ->
+          {files, spaces ++ [{idx, :empty, nil, space}]}
 
-          last = Enum.at(acc, last_idx)
+        {idx, {:movable, file, size}}, {files, spaces} ->
+          {files ++ [{idx, :movable, file, size}], spaces}
 
-          # Now uses a list with those values swaped.
-          List.replace_at(acc, idx, last)
-          |> List.replace_at(last_idx, nil)
-        else
+        _, acc ->
           acc
+      end)
+
+    updated =
+      Enum.reverse(files)
+      |> Enum.reduce_while(nil, fn {file_idx, :movable, file, size}, _ ->
+        case find_space_block(spaces, size) do
+          {space_idx, :empty, nil, space} ->
+            debug({file_idx, :movable, file, size})
+
+            if file_idx > space_idx do
+              {
+                :halt,
+                List.replace_at(blocks, file_idx, {:empty, nil, size})
+                |> List.replace_at(space_idx, [{:fixed, file, size}, {:empty, nil, space - size}])
+                |> List.flatten()
+              }
+            else
+              {:cont, blocks}
+            end
+
+          nil ->
+            {:cont, blocks}
         end
       end)
-      |> Enum.filter(&(&1 !== nil))
 
-    0..compressed_size
-    |> Enum.reduce(0, fn idx, acc ->
-      acc + idx * Enum.at(compressed, idx)
+    if updated === blocks do
+      updated
+    else
+      compress_blocks(updated)
+    end
+  end
+
+  defp find_space_block(spaces, size) do
+    Enum.find(spaces, fn
+      {_, :empty, nil, space} -> space >= size
     end)
   end
 end
